ac2f463c6389171b0a7c82fc15a90e4828a4dcac
Richard <richrd77@gmail.com>
2024-03-02 01:11:47 +0530

    view stuff

--- src/app/modules/mat/jatri-mat.module.ts
+++ src/app/modules/mat/jatri-mat.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 const matModule: any[] = [
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatDialogModule
 ];
 
 @NgModule({
--- src/app/modules/normal/components/register/register.component.ts
+++ src/app/modules/normal/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  @ViewChild('person', { read: TemplateRef }) private personTemplate: TemplateRef<any> | null;
+  @ViewChild('qr', { read: TemplateRef }) private qrTemplate: TemplateRef<any> | null;
 
+  constructor(private dialog: MatDialog) {
+    this.personTemplate = null;
+    this.qrTemplate = null;
+  }
+
+  public ShowPhoto(): void {
+    if (this.personTemplate) {
+      this.dialog.open<TemplateRef<any>>(this.personTemplate, {
+        maxWidth: '90vw',
+        maxHeight: '90vh',
+        height: '100%',
+        width: '100%'
+      })
+    }
+  }
+
+  public ShowQR(): void {
+    if (this.qrTemplate) {
+      this.dialog.open<TemplateRef<any>>(this.qrTemplate, {
+        maxWidth: '90vw',
+        maxHeight: '90vh',
+        height: '100%',
+        width: '100%'
+      })
+    }
+  }
 }
