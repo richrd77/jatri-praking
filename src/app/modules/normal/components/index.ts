6b0838965ace7de0ec96b2bae8a3e918a5386b95
Richard <richrd77@gmail.com>
2024-03-02 20:58:29 +0530

    checkout v1

--- src/app/modules/normal/components/index.ts
+++ src/app/modules/normal/components/index.ts
@@ -1,6 +1,15 @@
+import { CheckinComponent } from "./checkin/checkin.component";
 import { CheckoutComponent } from "./checkout/checkout.component";
+import { DetailFacadeComponent } from "./detail-facade/detail-facade.component";
 import { DetailsComponent } from "./details/details.component";
 import { QrReaderComponent } from "./qr-reader/qr-reader.component";
 import { RegisterComponent } from "./register/register.component";
 
-export { RegisterComponent, QrReaderComponent, DetailsComponent, CheckoutComponent }
+export {
+    RegisterComponent,
+    QrReaderComponent,
+    DetailsComponent,
+    CheckoutComponent,
+    CheckinComponent,
+    DetailFacadeComponent
+}
--- src/app/modules/normal/normal.module.ts
+++ src/app/modules/normal/normal.module.ts
@@ -5,23 +5,24 @@ import { JatriCommonModue } from "../common/common.module";
 import { CommonModule } from "@angular/common";
 import {
     RegisterComponent, DetailsComponent,
-    QrReaderComponent, CheckoutComponent
+    QrReaderComponent, CheckoutComponent,
+    DetailFacadeComponent, CheckinComponent
 } from './components';
 import { ReactiveFormsModule } from "@angular/forms";
 import { LOAD_WASM, NgxScannerQrcodeModule } from "ngx-scanner-qrcode";
 import { SafeImage } from "../common/pipes/safe-image.pipe";
-import { DetailFacadeComponent } from './components/detail-facade/detail-facade.component';
 
 const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'checkout', component: CheckoutComponent },
+    { path: 'checkin', component: CheckinComponent },
     { path: '**', component: HomeComponent },
 ];
 
 LOAD_WASM().subscribe();
 
 @NgModule({
-    declarations: [HomeComponent, RegisterComponent, QrReaderComponent, DetailsComponent, CheckoutComponent, DetailFacadeComponent],
+    declarations: [HomeComponent, RegisterComponent, QrReaderComponent, DetailsComponent, CheckoutComponent, DetailFacadeComponent, CheckinComponent],
     exports: [RouterModule],
     imports: [JatriCommonModue, CommonModule,
         RouterModule.forChild(routes), ReactiveFormsModule,
