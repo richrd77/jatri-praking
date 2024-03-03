03ae03828d75cbf0d920a152b32151075bf29d40
Richard <richrd77@gmail.com>
2024-03-03 17:58:49 +0530

    row event

--- src/app/modules/admin/components/helpers/admin-home-helper.service.ts
+++ src/app/modules/admin/components/helpers/admin-home-helper.service.ts
@@ -3,8 +3,8 @@ import { BehaviorSubject, Observable } from "rxjs";
 import { UserDetail } from "../../../normal/models";
 
 @Injectable()
-export class AdminHomeHelperService{
-    private data:UserDetail[]=[
+export class AdminHomeHelperService {
+    private data: UserDetail[] = [
         new UserDetail(),
         new UserDetail(),
         new UserDetail(),
@@ -27,13 +27,17 @@ export class AdminHomeHelperService{
         new UserDetail(),
         new UserDetail(),
     ];
-    private subject: BehaviorSubject<UserDetail[]>=new BehaviorSubject<UserDetail[]>([]);
+    private subject: BehaviorSubject<UserDetail[]> = new BehaviorSubject<UserDetail[]>([]);
 
-    public get data$(): Observable<UserDetail[]>{
+    public get data$(): Observable<UserDetail[]> {
         return this.subject.asObservable();
     }
 
     public Fetch(): void {
         this.subject.next(this.data);
+    }
+
+    public ShowDetail(row: any): void {
+        console.log('clicked', row);
     }
 }
